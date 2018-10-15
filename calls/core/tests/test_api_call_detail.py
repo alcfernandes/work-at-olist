4d1435c7d2f7b82ac1e4767e624dce1c6c30181a
Alessandro Fernandes <alcfernandes@yahoo.com>
2018-10-15 17:26:46 -0300

    Implements call-detail record validation by type
    Validation of source and destination fields according to the record type
    (start/end)

--- calls/core/tests/test_api_call_detail.py
+++ calls/core/tests/test_api_call_detail.py
@@ -74,3 +74,150 @@ class APIEndCallDetailCreate(APITestCase):
             str(self.response.content, encoding='utf8'),
             expected_response
         )
+
+
+class APIStartTypeCallsDetailsValidationTest(APITestCase):
+
+    def test_on_start_type_calls_details_source_is_required(self):
+        """
+        A Call Start Record without a source telephone number must be rejected.
+        """
+
+        payload = {
+            'id': 1,
+            'type': CallDetail.START,
+            'timestamp': "2016-02-29T12:00:00Z",
+            'destination': "9933468278",
+            'call_id': 70,
+        }
+
+        expected_response = {
+            "validation_error": [
+                "A start-type call detail record must have a source telephone number."
+            ]
+        }
+
+        response = self.client.post('/api/call-detail/', payload)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        self.assertJSONEqual(
+            str(response.content, encoding='utf8'),
+            expected_response
+        )
+
+    def test_on_start_type_calls_details_destination_is_required(self):
+        """
+        A Call Start Record without a destination telephone number must be rejected.
+        """
+
+        payload = {
+            'id': 1,
+            'type': CallDetail.START,
+            'timestamp': "2016-02-29T12:00:00Z",
+            'source': "9933468278",
+            'call_id': 70,
+        }
+
+        expected_response = {
+            "validation_error": [
+                "A start-type call detail record must have a destination telephone number."
+            ]
+        }
+
+        response = self.client.post('/api/call-detail/', payload)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        self.assertJSONEqual(
+            str(response.content, encoding='utf8'),
+            expected_response
+        )
+
+
+class APIEndTypeCallsDetailsValidationTest(APITestCase):
+
+    def test_on_end_type_calls_details_source_and_destination_are_not_required(self):
+        """
+        For an end-type call detail record, source and destination fields should not be required
+        """
+
+        payload = {
+            'id': 1,
+            'type': CallDetail.END,
+            'timestamp': "2016-02-29T12:00:00Z",
+            'call_id': 70,
+        }
+
+        expected_response = {
+            'url': 'http://testserver/api/call-detail/1/',
+            'id': 1,
+            'type': CallDetail.END,
+            'timestamp': "2016-02-29T12:00:00Z",
+            'source': None,
+            'destination': None,
+            'call_id': 70
+        }
+
+        response = self.client.post('/api/call-detail/', payload)
+
+        self.assertJSONEqual(
+            str(response.content, encoding='utf8'),
+            expected_response
+        )
+
+    def test_on_end_type_calls_details_source_is_not_expected(self):
+        """
+        A end-type Call Detail Record should not have a source telephone number.
+        """
+
+        payload = {
+            'id': 1,
+            'type': CallDetail.END,
+            'timestamp': "2016-02-29T12:00:00Z",
+            'source': "9933468278",
+            'call_id': 70,
+        }
+
+        expected_response = {
+            "validation_error": [
+                "A end-type call detail record should not have a source telephone number."
+            ]
+        }
+
+        response = self.client.post('/api/call-detail/', payload)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        self.assertJSONEqual(
+            str(response.content, encoding='utf8'),
+            expected_response
+        )
+
+    def test_on_end_type_calls_details_destination_is_not_expected(self):
+        """
+        A end-type Call Detail Record should not have a destination telephone number.
+        """
+
+        payload = {
+            'id': 1,
+            'type': CallDetail.END,
+            'timestamp': "2016-02-29T12:00:00Z",
+            'destination': "9933468278",
+            'call_id': 70,
+        }
+
+        expected_response = {
+            "validation_error": [
+                "A end-type call detail record should not have a destination telephone number."
+            ]
+        }
+
+        response = self.client.post('/api/call-detail/', payload)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        self.assertJSONEqual(
+            str(response.content, encoding='utf8'),
+            expected_response
+        )
